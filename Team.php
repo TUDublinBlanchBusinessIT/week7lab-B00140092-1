7563a7a0384d2519e8a052709e516710affa244d
unknown <B00140092@mytudublin.ie>
2023-11-08 10:33:01 +0000

    finished part two, made a function that gets the average goals

--- Team.php
+++ Team.php
@@ -1,15 +1,18 @@
 <?php
-class Team {
-    private $name;
+class Team 
+{
+    private $name="";
     private $totalPoints = 0;
     private $totalGames = 0;
     private $totalGoals = 0;
 
-    public function __construct($name) {
+    public function __construct($name) 
+	{
         $this->name = $name;
     }
 
-    public function finalScore($homeTeamScore, $opposingTeamScore) {
+    public function finalScore($homeTeamScore, $opposingTeamScore) 
+	{
         $this->totalGames++;
         $this->totalGoals += $homeTeamScore;
 
@@ -21,5 +24,14 @@ class Team {
 
 
     }
+		public function getGoalAverage() 
+	{
+			if ($this->totalGames == 0) {
+				return 0.0;
+			}
+			return $this->totalGoals / $this->totalGames;
+		}
+
 }
+
 ?>
